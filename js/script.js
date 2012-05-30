76c4f88068d01b9df0455e93a4d197604fa8136f
ernie <ernie@epicbytes.net>
2012-05-31 00:07:50 +0200

    new icons ++

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function display_results(msg) {
     output += "<li>Frakt<div class=\"ui-li-count\">"+converted_shipping.toFixed(2)+"</div></li>";
     output += "<li>Gebyr<div class=\"ui-li-count\">"+fee+"</div></li>";
     output += "<li>MVA<div class=\"ui-li-count\">"+vat.toFixed(2)+"</div></li>";
-    output += "<li>Total<div class=\"ui-li-count\">"+total.toFixed(2)+"</div></li>";
+    output += "<li>Totalt<div class=\"ui-li-count\">"+total.toFixed(2)+"</div></li>";
     
     $.mobile.changePage("#results");
 
