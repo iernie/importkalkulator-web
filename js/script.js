793324f671e15baf492a45aa5772a82f022ebda3
ernie <ernie@epicbytes.net>
2012-05-30 22:39:04 +0200

    bugfix2

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@ function display_results(msg) {
     var output = "";
     output += "<li>Kurs<div class=\"ui-li-count\">"+rate+"</div></li>";
     
-    $('#listview').html(output).listview();
+    $('#listview').html(output);
+    $('#listview').listview("refresh"); 
     $.mobile.changePage("#results");
 }
 
